111d5e200cfa2cee3148b4c56546640ef7cfab44
Kalovelo <apostoloskalovelonis@gmail.com>
2023-01-21 16:32:10 +0200

    faat: add linter

--- blog-app/pages/api/socket.ts
+++ blog-app/pages/api/socket.ts
@@ -1,14 +1,14 @@
-import { Server } from 'Socket.IO'
+import { Server } from 'Socket.IO';
 
 const SocketHandler = (_req: Request, res) => {
   if (res.socket.server.io) {
-    console.log('Socket is already running')
+    console.log('Socket is already running');
   } else {
-    console.log('Socket is initializing')
-    const io = new Server(res.socket.server)
-    res.socket.server.io = io
+    console.log('Socket is initializing');
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
   }
-  res.end()
-}
+  res.end();
+};
 
-export default SocketHandler
+export default SocketHandler;
--- blog-app/pages/index.tsx
+++ blog-app/pages/index.tsx
@@ -1,14 +1,14 @@
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
-import Post from "../interfaces/post";
-import { useEffect } from "react";
-import io, { Socket } from "Socket.IO-client";
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
+import Head from 'next/head';
+import { CMS_NAME } from '../lib/constants';
+import Post from '../interfaces/post';
+import { useEffect } from 'react';
+import io, { Socket } from 'Socket.IO-client';
 
 let socket: Socket;
 
@@ -23,11 +23,11 @@ export default function Index({ allPosts }: Props) {
   useEffect(() => void socketInitializer(), []);
 
   const socketInitializer = async () => {
-    await fetch("/api/socket");
+    await fetch('/api/socket');
     socket = io();
 
-    socket.on("connect", () => {
-      console.log("connected");
+    socket.on('connect', () => {
+      console.log('connected');
     });
   };
 
@@ -57,7 +57,7 @@ export default function Index({ allPosts }: Props) {
 }
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts(["title", "date", "slug", "author", "coverImage", "excerpt"]);
+  const allPosts = getAllPosts(['title', 'date', 'slug', 'author', 'coverImage', 'excerpt']);
 
   return {
     props: { allPosts },

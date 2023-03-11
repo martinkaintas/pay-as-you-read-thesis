c1102fbae8a932ff0ed8e25f6c9f9f9d74d5c589
martinkaintas <martinkaintas@gmail.com>
2023-03-11 15:32:31 +0200

    temp

--- blog-app/pages/api/socket.ts
+++ blog-app/pages/api/socket.ts
@@ -10,11 +10,28 @@ const SocketHandler = (_req: Request, res) => {
     io = new Server(res.socket.server);
     res.socket.server.io = io;
   }
+  io.on('connection', (socket) => {
+    socket.on('buyParagraph', async (data: { postId: string; index: number }) => {
+      console.log(`client wants to buy paragraph ${data.index} of post ${data.postId}`);
+      io.emit(
+        'invoice',
+        await invoiceService.createInvoice(
+          100,
+          `paragraph ${data.index.toString()} of post ${data.postId}`,
+        ),
+      );
+      invoiceService.paymentListener.on('confirmed', (data) => {
+        console.log('payment received', data);
+      });
+      invoiceService.paymentListener.on('paying', (data) => {
+        console.log('paying', data);
+      });
+    });
+    socket.on('disconnect', () => console.log('Client disconnected'));
+  });
 
   res.end();
-  invoiceService.on('connected', async () =>
-    io.emit('invoice', await invoiceService.createInvoice(1000, 'YEAAAAH BUDDY')),
-  );
+  invoiceService.on('connected', async () => console.log('Connected to LND'));
 };
 
 export default SocketHandler;
